cebaeec015106c2e3f6f89cf7db6f2380eb25d66
Nikolay <vulkank@mail.ru>
2024-09-10 19:39:52 +0300

    last

--- hh_scrape/hh_scrape/spiders/hh.py
+++ hh_scrape/hh_scrape/spiders/hh.py
@@ -1,6 +1,7 @@
 
 import scrapy
 from hh_scrape.items import HhScrapeItem
+from scrapy.http import HtmlResponse
 
 class HhSpider(scrapy.Spider):
     name = "hh"
@@ -8,7 +9,7 @@ class HhSpider(scrapy.Spider):
         'https://hh.ru/search/vacancy?text=%D0%B4%D0%B0%D1%82%D0%B0-%D0%B8%D0%BD%D0%B6%D0%B5%D0%BD%D0%B5%D1%80&salary=&ored_clusters=true&hhtmFrom=vacancy_search_list&hhtmFromLabel=vacancy_search_line&page=0'
     ]
 
-    def parse(self, response):
+    def parse(self, response:HtmlResponse):
         base_url = response.url.split('&page=')[0]
         QUANTITY_PAGES = 6
         
@@ -17,7 +18,7 @@ class HhSpider(scrapy.Spider):
             self.logger.info(f'URL страницы: {url}')
             yield scrapy.Request(url=url, callback=self.parse_page)
 
-    def parse_page(self, response):
+    def parse_page(self, response:HtmlResponse):
         self.logger.info(f'Обрабатываем страницу: {response.url}')
         # Найти все элементы вакансий на странице
         vacancies = response.xpath('//div[contains(@class, "magritte-redesign")]')
@@ -26,7 +27,7 @@ class HhSpider(scrapy.Spider):
             title = vacancy.xpath('.//span[@data-qa="serp-item__title-text"]/text()').get()
             href = vacancy.xpath('.//a[@data-qa="serp-item__title"]/@href').get()
             solary = vacancy.xpath(".//span[@class='magritte-text___pbpft_3-0-14 magritte-text_style-primary___AQ7MW_3-0-14 magritte-text_typography-label-1-regular___pi3R-_3-0-14']//text()").getall()
-            
+            solary = list(dict.fromkeys([s.strip() for s in solary if s.strip()]))  # Удаляем пробелы и дубликаты 
             # Логируем и сохраняем данные
             self.logger.info(f'Заголовок вакансии: {title}')     
             self.logger.info(f'Ссылка вакансии: {href}')     
