71c5b6c4eedd1005711e51671fd50080d2572f01
Nikolay <vulkank@mail.ru>
2024-09-10 13:54:19 +0300

    hh_scrapy

---
+++ hh_scrape/hh_scrape/pipelines.py
@@ -0,0 +1,22 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from pymongo import MongoClient
+
+class HhScrapePipeline:
+    def __init__(self) -> None:
+        client = MongoClient('localhost', 27017)
+        self.mongo_base = client.vacancies100924
+
+    def process_item(self, item, spider):
+        # Добавляем каждый элемент (данные с вакансий) в список
+        # self.data.append(item)
+        collection = self.mongo_base[spider.name]
+        collection.insert_one(item)
+        return item
+
+
---
+++ hh_scrape/hh_scrape/spiders/hh.py
@@ -0,0 +1,94 @@
+
+# import scrapy
+# import json
+
+# class HhSpider(scrapy.Spider):
+#     name = "hh"
+#     start_urls = [
+#         'https://hh.ru/search/vacancy?text=%D0%B4%D0%B0%D1%82%D0%B0-%D0%B8%D0%BD%D0%B6%D0%B5%D0%BD%D0%B5%D1%80&salary=&ored_clusters=true&hhtmFrom=vacancy_search_list&hhtmFromLabel=vacancy_search_line&page=0'
+#     ]
+
+#     def parse(self, response):
+#         base_url = response.url.split('&page=')[0]
+#         QUANTITY_PAGES = 6
+        
+#         for i in range(QUANTITY_PAGES):
+#             url = f'{base_url}&page={i}'
+#             self.logger.info(f'URL страницы: {url}')
+#             yield {'page_url': url}
+
+#     def start_requests(self):
+#         # Открываем JSON-файл и загружаем список URL-ов
+#         with open('vacancies.json', 'r') as file:
+#             pages = json.load(file)
+
+#         # Проходим по каждому URL и делаем запрос с помощью Scrapy
+#         for page in pages:
+#             url = page['page_url']
+#             yield scrapy.Request(url=url, callback=self.parse_page)
+
+#     def parse_page(self, response):
+#         # Обрабатываем ответ от страницы
+#         self.logger.info(f'Обрабатываем страницу: {response.url}')
+#         # Здесь можно извлекать данные и выполнять другие действия
+#         # Например, извлечь заголовок страницы:
+#         TITLE = '//a[@class="magritte-link___b4rEM_4-2-10"]/text()'
+#         HREF = '//a[@class="magritte-link___b4rEM_4-2-10"]/@href'
+#         SOLARY = '//span[@class="magritte-text___pbpft_3-0-14"]/text()'
+#         EXPERTISE = '//div[@class="magritte-tag___WdGxk_3-0-11"]/text()'
+
+#         title = response.xpath('//span[@data-qa="serp-item__title-text"]/text()').get()
+#         title1 = response.xpath('//a[@data-qa="vacancy-serp__vacancy-title"]/text()').get()
+
+#         # href = response.xpath("//a[@data-qa='serp-item_title']/@href").get()
+#         # solary = response.xpath(SOLARY).get()
+#         # expertise = response.xpath(EXPERTISE).get()
+
+#         self.logger.info(f'Заголовок страницы: {title}')     
+#         self.logger.info(f'Ссылка страницы: {title1}')     
+
+#         yield {
+#             'title': title,
+#             # 'href': href,
+#             # 'salary': salary,
+#             # 'expertise': expertise,
+#             'page_url': response.url
+#         }
+
+
+import scrapy
+from hh_scrape.items import HhScrapeItem
+
+class HhSpider(scrapy.Spider):
+    name = "hh"
+    start_urls = [
+        'https://hh.ru/search/vacancy?text=%D0%B4%D0%B0%D1%82%D0%B0-%D0%B8%D0%BD%D0%B6%D0%B5%D0%BD%D0%B5%D1%80&salary=&ored_clusters=true&hhtmFrom=vacancy_search_list&hhtmFromLabel=vacancy_search_line&page=0'
+    ]
+
+    def parse(self, response):
+        base_url = response.url.split('&page=')[0]
+        QUANTITY_PAGES = 6
+        
+        for i in range(QUANTITY_PAGES):
+            url = f'{base_url}&page={i}'
+            self.logger.info(f'URL страницы: {url}')
+            yield scrapy.Request(url=url, callback=self.parse_page)
+
+    def parse_page(self, response):
+        self.logger.info(f'Обрабатываем страницу: {response.url}')
+        # Найти все элементы вакансий на странице
+        vacancies = response.xpath('//div[contains(@class, "magritte-redesign")]')
+
+        for vacancy in vacancies:
+            title = vacancy.xpath('.//span[@data-qa="serp-item__title-text"]/text()').get()
+            href = vacancy.xpath('.//a[@data-qa="serp-item__title"]/@href').get()
+            solary = vacancy.xpath(".//span[@class='magritte-text___pbpft_3-0-14 magritte-text_style-primary___AQ7MW_3-0-14 magritte-text_typography-label-1-regular___pi3R-_3-0-14']//text()").getall()
+            
+            # Логируем и сохраняем данные
+            self.logger.info(f'Заголовок вакансии: {title}')     
+            self.logger.info(f'Ссылка вакансии: {href}')     
+            self.logger.info(f'Зарплата: {solary}')     
+
+            yield HhScrapeItem(title=title,href=href,solary=solary)
+
+       
